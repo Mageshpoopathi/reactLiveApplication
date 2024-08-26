d78116bb09d3862730d14fd6e3414105eae03795
Mageshpoopathi <mageshpoopathi208@gmail.com>
2024-08-26 21:33:30 +0530

    Add registration page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 
 import './App.css';
-import Form from './component/Form';
+import Registrationpage from './component/Registrationpage';
 
 function App() {
   return (
     <div className="App">
-      <h2>Login page</h2>
-      <h2>Login page1</h2>
-      <h2>Login page2</h2>
-      <h2>Login page3</h2>
-      <h5>For Every User</h5>
-     <Form/>
+      <Registrationpage/>
     </div>
   );
 }
 
 export default App;
+
